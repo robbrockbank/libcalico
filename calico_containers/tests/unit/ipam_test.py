950dbeb5326012c77531f429aa1c5dff45b728a7
Luke Mino-Altherr <luke.mino-altherr@metaswitch.com>
2015-08-20 15:23:09 -0700

    typo

--- calico_containers/tests/unit/ipam_test.py
+++ calico_containers/tests/unit/ipam_test.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from netaddr import IPNetwork, IPAddres
+from netaddr import IPNetwork, IPAddress
 from nose.tools import *
 from mock import patch, ANY, call, Mock
 import unittest
